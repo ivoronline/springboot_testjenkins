945a64d647ae6c86de8ea740f0a2c09e329fdb26
igoreta <ivoronline@gmail.com>
2021-02-10 12:55:02 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_testjenkins/SpringbootTestjenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_testjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestjenkinsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootTestjenkinsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_testjenkins/SpringbootTestjenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_testjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestjenkinsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
