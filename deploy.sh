c2c7ccae98bc8ffb29e25f20b90ca26537655308
igoreta <ivoronline@gmail.com>
2021-02-13 11:42:28 +0100

    Commit

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+# KILL APPLICATION
+pgrep -f 'springboot_testjenkins-0.0.1-SNAPSHOT.jar' | xargs kill
+
+# REMOVE JAR
+rm /home/igoreta/applications/TestJenkins/springboot_testjenkins-0.0.1-SNAPSHOT.jar
+
+# COPY JAR
+cp /var/lib/jenkins/.m2/repository/com/ivoronline/springboot_testjenkins/0.0.1-SNAPSHOT/ springboot_testjenkins-0.0.1-SNAPSHOT.jar /home/igoreta/applications/TestJenkins/
